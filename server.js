19ab7e351e9cb3a428dfbfb369d30184b81d5e1e
Hugo Zhan <hugozhan0802@gmail.com>
2024-03-21 10:48:36 -0700

    back to bnefore

--- server.js
+++ server.js
@@ -5,11 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const corsOptions = {
-  origin: 'https://frontend-livid-six-25.vercel.app',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
